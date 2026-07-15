cf8333b03a3d1c4c60125d3e33a4d58312f746f6
shanmukh-JS <3shanmukhkadali@gmail.com>
2026-07-15 12:16:01 +0530

    fix: removed --env-file from package.json and added dotenv programmatically to prevent Render crash

--- apps/web/workers/index.ts
+++ apps/web/workers/index.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' })
 import { connection } from './queues'
 import { settlementWorker } from './settlementWorker'
 import { payoutBatchWorker } from './payoutBatchWorker'
